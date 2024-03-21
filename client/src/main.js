9c22c49efe88fa78e008aa9a45f90040fd36d353
MohdFahd <mohammed.f.alsulami@gmail.com>
2024-03-21 14:34:39 +0300

    install tailwind ,Update styles and dependencies

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,4 @@
-import "./assets/main.css";
+import "./assets/style.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,29 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <el-row class="bg-slate-600">
+    <el-col :span="12"
+      ><div class="grid-content ep-bg-purple-dark" />
+      Header</el-col
+    >
+    <el-col :span="12"
+      ><div class="grid-content ep-bg-purple-dark" />
+      Header</el-col
+    >
+  </el-row>
 </template>
+
+<style lang="scss">
+.el-row {
+  margin-bottom: 20px;
+}
+.el-row:last-child {
+  margin-bottom: 0;
+}
+.el-col {
+  border-radius: 4px;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+</style>
