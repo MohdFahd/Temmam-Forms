9419a7e5453b0018f5d266b481672a09ad6ba1e0
MohdFahd <mohammed.f.alsulami@gmail.com>
2024-04-03 13:02:25 +0300

    Refactor Answers.vue component to use a table instead of el-table

--- client/src/views/Answers.vue
+++ client/src/views/Answers.vue
@@ -1,16 +1,46 @@
 <template>
-  <el-table
-    :data="Answers"
-    style="width: 100%"
-    :row-class-name="tableRowClassName"
-  >
-    <el-table-column
-      v-for="(value, key) in Answers[0]"
-      :key="key"
-      :prop="key"
-      :label="key"
-    />
-  </el-table>
+  <div class="relative overflow-x-auto">
+    <table
+      class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+    >
+      <thead
+        class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+      >
+        <tr>
+          <th scope="col" class="px-6 py-3">User</th>
+          <th
+            scope="col"
+            class="px-6 py-3"
+            v-for="question in questions"
+            :key="question"
+          >
+            {{ question }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="answer in Answers"
+          :key="answer"
+          class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+        >
+          <th
+            scope="row"
+            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+          >
+            {{ answer.user_name }}
+          </th>
+          <td
+            class="px-6 py-4"
+            v-for="qesAns in answer.questions"
+            :key="qesAns"
+          >
+            {{ qesAns.answers }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -19,10 +49,11 @@ import { ref, onMounted } from "vue";
 import axios from "axios";
 import Swal from "sweetalert2";
 
-const Answers = ref([]);
+const Answers = ref({});
 const $route = useRoute();
 const id = $route.params.id;
-const questions = ref([]);
+
+let questions = [];
 
 onMounted(() => {
   axios
@@ -31,31 +62,19 @@ onMounted(() => {
       // Update Answers with the retrieved data
       Answers.value = response.data.data;
 
-      // Extract unique question names
-      const uniqueQuestions = [
-        ...new Set(response.data.data.map((item) => item.questionName)),
-      ];
-      // Construct array of columns for table headers
-      questions.value = uniqueQuestions.map((questionName) => ({
-        id: questionName,
-        questionName,
-      }));
+      // Fetch all questionNames for the first user
+      const firstUser = Object.values(Answers.value)[0];
+      if (firstUser && firstUser.questions) {
+        questions = firstUser.questions.map(
+          (question) => question.questionName
+        );
+      }
     })
     .catch((error) => {
       console.error("Error:", error);
       // Handle error
     });
 });
-
-const tableRowClassName = ({
-  row,
-  rowIndex,
-}: {
-  row: any;
-  rowIndex: number;
-}) => {
-  // Add row class logic if needed
-};
 </script>
 
 <style></style>
